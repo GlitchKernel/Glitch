983d0a89d1a6a3eb48c3f885859dd84222f6bf57
Jason Stewart <sixstringsg@verizon.net>
2011-07-11 00:18:14 -0500

    Code for enable-logcat and disable-logcat by user
    To enable logcat, type logcat-on.sh then reboot. For disable, same except logcat-off.sh. Module loaded (not very cleanly) by screenstate_scaling

--- release/doit.sh
+++ release/doit.sh
@@ -12,20 +12,22 @@ TYPE=$1
 
 declare -i RELVER=0
 
-until test ! -s release/SGS/${REL}; do
+until test ! -s release/${TYPE}/${REL}; do
 REL=CM7_${TYPE}_Glitch-kernel_$(date +%Y%m%d_r)${RELVER}_update.zip
 RELVER+=1
 done
 
-
 rm -r release/system 2> /dev/null
 mkdir  -p release/system/lib/modules || exit 1
 # mkdir  -p release/system/lib/hw || exit 1
+cp release/logger.module release/system/lib/modules/logger.ko
 mkdir  -p release/system/etc/init.d || exit 1
 find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 
 cd release && {
 	cp 90screenstate_scaling system/etc/init.d/ || exit 1
+	mkdir -p system/bin
+	cp bin/* system/bin/
 #	cp lights.aries.so system/lib/hw/ || exit 1
 	zip -q -r ${REL} system boot.img META-INF bml_over_mtd bml_over_mtd.sh || exit 1
 	sha256sum ${REL} > ${REL}.sha256sum
@@ -33,8 +35,8 @@ cd release && {
 	mv ${REL}* ${TYPE} || exit 1
 } || exit 1
 
+echo ${REL}
 rm system/lib/modules/*
 #rm system/lib/hw/*
 rm system/etc/init.d/*
-echo ${REL}
 exit 0
--- release/doit_cappy.sh
+++ release/doit_cappy.sh
@@ -10,21 +10,25 @@ echo "packaging it up"
 TYPE=$1
 [[ "$TYPE" == '' ]] && TYPE=CAPPY
 
-RELVER=$2
-[[ "$RELVER" == '' ]] && RELVER="0"
+declare -i RELVER=0
 
+until test ! -s release/${TYPE}/${REL}; do
 REL=CM7_${TYPE}_Glitch-kernel_$(date +%Y%m%d_r)${RELVER}_update.zip
+RELVER+=1
+done
 
 rm -r release/system 2> /dev/null
 mkdir  -p release/system/lib/modules || exit 1
 mkdir  -p release/system/etc/init.d || exit 1
+cp release/logger.module release/system/lib/modules/logger.ko
 find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 
 cd release && {
 	cp 90screenstate_scaling system/etc/init.d/ || exit 1
+	mkdir -p system/bin
+	cp bin/* system/bin/
 	zip -q -r ${REL} system boot.img META-INF bml_over_mtd bml_over_mtd.sh || exit 1
 	sha256sum ${REL} > ${REL}.sha256sum
-	rm -rf ${TYPE} || exit 1
 	mkdir -p ${TYPE} || exit 1
 	mv ${REL}* ${TYPE} || exit 1
 } || exit 1
