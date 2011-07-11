77040d5b264f1dd7cf2a8d51f7431875206407c4
Jason Stewart <sixstringsg@verizon.net>
2011-07-11 16:21:41 -0500

    More logcat
    Makes logcat cleaner, adding it's own init.d script, reverting screenstate_scaling, and modifying doit_cappy.sh and doit.sh to add logcat_module script to init.d

--- release/doit.sh
+++ release/doit.sh
@@ -20,14 +20,12 @@ done
 rm -r release/system 2> /dev/null
 mkdir  -p release/system/lib/modules || exit 1
 # mkdir  -p release/system/lib/hw || exit 1
-cp release/logger.module release/system/lib/modules/logger.ko
 mkdir  -p release/system/etc/init.d || exit 1
 find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 
 cd release && {
+	cp logcat_module system/etc/init.d/ || exit 1
 	cp 90screenstate_scaling system/etc/init.d/ || exit 1
-	mkdir -p system/bin
-	cp bin/* system/bin/
 #	cp lights.aries.so system/lib/hw/ || exit 1
 	zip -q -r ${REL} system boot.img META-INF bml_over_mtd bml_over_mtd.sh || exit 1
 	sha256sum ${REL} > ${REL}.sha256sum
