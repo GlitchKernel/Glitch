3fab6129681da783d4c2f2bee7bdf26f9434655e
Tk-Glitch <Ti3noU@gmail.com>
2011-07-16 17:06:29 +0200

    initramfs building script - temporary disabled fascinate building

--- initramfs.sh
+++ initramfs.sh
@@ -4,16 +4,16 @@ cd ../../../
 
 echo "Building initramfs files for all devices" && {
 
-echo "-------------------------Building for i9000"
+echo "--------------------Building for i9000------------------"
 . build/envsetup.sh && lunch full_galaxysmtd-eng && make bootimage
 
-echo "-------------------------Building for Captivate"
+echo "------------------Building for Captivate----------------"
 . build/envsetup.sh && lunch full_captivatemtd-eng && make bootimage
 
-echo "-------------------------Building for Vibrant"
+echo "-------------------Building for Vibrant-----------------"
 . build/envsetup.sh && lunch full_vibrantmtd-eng && make bootimage
 
-echo "-------------------------Building for Fascinate"
+#echo "------------------Building for Fascinate----------------"
 #. build/envsetup.sh && lunch full_fascinatemtd-eng && make bootimage
 }
 
