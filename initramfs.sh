a3c4531e1a1c3043db88a257fd81497f82e3bf61
Tk-Glitch <Ti3noU@gmail.com>
2011-07-16 17:02:52 +0200

    Still fascinate work + some slight tweaks

---
+++ initramfs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ../../../
+
+echo "Building initramfs files for all devices" && {
+
+echo "-------------------------Building for i9000"
+. build/envsetup.sh && lunch full_galaxysmtd-eng && make bootimage
+
+echo "-------------------------Building for Captivate"
+. build/envsetup.sh && lunch full_captivatemtd-eng && make bootimage
+
+echo "-------------------------Building for Vibrant"
+. build/envsetup.sh && lunch full_vibrantmtd-eng && make bootimage
+
+echo "-------------------------Building for Fascinate"
+#. build/envsetup.sh && lunch full_fascinatemtd-eng && make bootimage
+}
+
+echo "Done!"
