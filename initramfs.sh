dfeee9cf29ca4d453886e2a14e2e3d010de09055
Tk-Glitch <Ti3noU@gmail.com>
2011-07-16 20:32:06 +0200

    better initramfs build script

--- initramfs.sh
+++ initramfs.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
+# Vibrant needs a fix on CM7 repo for now. Navigate to device/samsung/vibrantmtd/, and open full_vibrantmtd.mk. On line 34, PRODUCT_DEVICE := SGH-T959 should be changed to PRODUCT_DEVICE := vibrantmtd.
+
+# Building for all devices by default.
+
+# To build for one device only, use ./initramfs.sh "phone name" (without quote).
+# Example : ./initramfs.sh captivate
+
+declare -A phone
+
+if phones[0]=$1; then
+echo ""
+echo "--------------Forcing for $1 only"
+
+else
+
+phones[0]="galaxys"
+phones[1]="captivate"
+phones[2]="vibrant"
+phones[3]="fascinate"
+
+fi
+
 cd ../../../
 
-echo "Building initramfs files for all devices" && {
+for i in ${!phones[@]}; do
+	phone=${phones[$i]}
 
-echo "--------------------Building for i9000------------------"
-. build/envsetup.sh && lunch full_galaxysmtd-eng && make bootimage
-
-echo "------------------Building for Captivate----------------"
-. build/envsetup.sh && lunch full_captivatemtd-eng && make bootimage
-
-echo "-------------------Building for Vibrant-----------------"
-. build/envsetup.sh && lunch full_vibrantmtd-eng && make bootimage
-
-#echo "------------------Building for Fascinate----------------"
-#. build/envsetup.sh && lunch full_fascinatemtd-eng && make bootimage
-}
+echo "--------------Building ramdisk.img and recovery.img for ${phone}"
+echo ""
+. build/envsetup.sh && lunch full_${phone}mtd-eng && make bootimage
+done
 
 echo "Done!"
