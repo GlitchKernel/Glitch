9dadf0a40411c081d5f83d35c9f49e7bac52c8cd
Tk-Glitch <Ti3noU@gmail.com>
2011-07-18 15:02:04 +0200

    fixing initramfs script

--- initramfs.sh
+++ initramfs.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# Vibrant needs a fix on CM7 repo for now. Navigate to device/samsung/vibrantmtd/, and open full_vibrantmtd.mk. On line 34, PRODUCT_DEVICE := SGH-T959 should be changed to PRODUCT_DEVICE := vibrantmtd.
+# Vibrant needs a fix on CM7 repo for now. Navigate to device/samsung/vibrantmtd/, and open full_vibrantmtd.mk. On line 34, PRODUCT_DEVICE := SGH-T959 should be changed to PRODUCT_DEVICE := vibrantmtd
 
-# Building for all devices by default.
+# This script will build for all listed devices by default
 
-# To build for one device only, use ./initramfs.sh "phone name" (without quote).
+# To build for one device only, use ./initramfs.sh "phone name" (without quote)
 # Example : ./initramfs.sh captivate
 
 declare -A phone
 
-if phones[0]=$1; then
-echo ""
-echo "--------------Forcing for $1 only"
+if [[ $# -gt 0 ]]; then
+	phones[0]=$1
+	echo " "
+	echo "================Force building for $1 only=================="
 
 else
-
-phones[0]="galaxys"
-phones[1]="captivate"
-phones[2]="vibrant"
-phones[3]="fascinate"
-
+	phones[0]="galaxys"
+	phones[1]="captivate"
+	phones[2]="vibrant"
+	phones[3]="fascinate"
 fi
 
 cd ../../../
@@ -27,7 +26,7 @@ cd ../../../
 for i in ${!phones[@]}; do
 	phone=${phones[$i]}
 
-echo "--------------Building ramdisk.img and recovery.img for ${phone}"
+echo "========Building ramdisk.img and recovery.img for ${phone}========"
 echo ""
 . build/envsetup.sh && lunch full_${phone}mtd-eng && make bootimage
 done
