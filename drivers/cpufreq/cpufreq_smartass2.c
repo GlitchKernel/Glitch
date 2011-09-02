b952d0a2d572df463d38faa5ba7f0f1c3f9d7d58
Matt B <jackdachef@gmail.com>
2011-09-02 22:22:09 +0200

    fixed a small nitpick in smartass2 name

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -165,7 +165,7 @@ static int cpufreq_governor_smartass2(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_smartass2 = {
-	.name = "smartass2V2",
+	.name = "smartassV2",
 	.governor = cpufreq_governor_smartass2,
 	.max_transition_latency = 6000000,
 	.owner = THIS_MODULE,
