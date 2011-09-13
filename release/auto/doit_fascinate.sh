0dce3f24164de45d9b0a9af5ab73ccb6740c2440
Bill Crossley <billcrossley@gmail.com>
2011-09-13 14:25:26 -0400

    updated scripts for making the zip

--- release/auto/doit_fascinate.sh
+++ release/auto/doit_fascinate.sh
@@ -27,11 +27,28 @@ REL=CM7${TYPE}-Glitch-DEV-$(date +%Y%m%d_%H%M).zip
 	cd ../
 		find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 	cd release
-	cp 90screenstate_scaling system/etc/init.d/ || exit 1
-	cp lights.aries.so system/lib/hw/ || exit 1
+	cp 90screenstate_scaling system/etc/init.d/ || exit 1	
 	cp logcat_module system/etc/init.d/ || exit 1
 	mkdir -p system/bin
 	cp bin/* system/bin/
+
+	cp lights.default.so system/lib/hw
+	cp sensors.default.so system/lib/hw
+	
+	# optional folders to go into system
+	if [ -d app ]; then
+		cp -r app system || exit 1
+	fi
+	
+	# stuff in vendor (apparently) overrides other stuff
+	if [ -d vendor ]; then
+		cp -r vendor system || exit 1
+	fi
+
+	if [ -d lib ]; then
+		cp -r lib system || exit 1
+	fi
+	
 	zip -q -r ${REL} system boot.img META-INF script bml_over_mtd bml_over_mtd.sh || exit 1
 	sha256sum ${REL} > ${REL}.sha256sum
 	mv ${REL}* ${TYPE} || exit 1
