3a5a3ff10ed8df7d7dcd253bf358dc224e00c2a0
Bill Crossley <billcrossley@gmail.com>
2011-11-07 13:17:50 -0500

    don't artificially limit UV to -50mv

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -699,8 +699,9 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
      }
 #endif
 
-	if(unlikely(exp_UV_mV[index] < -50)) 
-		exp_UV_mV[index] = -50;
+  // don't artificially limit UV
+	//if(unlikely(exp_UV_mV[index] < -50)) 
+	//	exp_UV_mV[index] = -50;
 		
 	arm_volt = (dvs_conf[index].arm_volt - (exp_UV_mV[index]*1000));
 
@@ -947,8 +948,9 @@ switch(s3c_freqs.old.armclk) {
 	cpufreq_debug_printk(CPUFREQ_DEBUG_DRIVER, KERN_INFO,
 			"cpufreq: Performance changed[L%d]\n", index);
 	
-	if(unlikely(exp_UV_mV[index] < -50)) 
-		exp_UV_mV[index] = -50;
+	// don't artificially limit uv
+	//if(unlikely(exp_UV_mV[index] < -50)) 
+	//  exp_UV_mV[index] = -50;
 
 	previous_arm_volt = (dvs_conf[index].arm_volt - (exp_UV_mV[index] * 1000));
 	//freq_uv_table[index][2] = (int) previous_arm_volt / 1000;
@@ -994,9 +996,10 @@ static int s5pv210_cpufreq_resume(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-			
-	if(unlikely(exp_UV_mV[level] < -50)) 
-		exp_UV_mV[level] = -50;
+	
+	// don't artificially limit UV		
+	//if(unlikely(exp_UV_mV[level] < -50)) 
+	//	exp_UV_mV[level] = -50;
 
 	previous_arm_volt = (dvs_conf[level].arm_volt - (exp_UV_mV[level]*1000));
 
@@ -1066,9 +1069,10 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-			
-	if(unlikely(exp_UV_mV[level] < -50)) 
-		exp_UV_mV[level] = -50;
+		
+	// don't artificially limit UV	
+	//if(unlikely(exp_UV_mV[level] < -50)) 
+	//	exp_UV_mV[level] = -50;
 
 	previous_arm_volt = (dvs_conf[level].arm_volt - (exp_UV_mV[level]*1000));
 
