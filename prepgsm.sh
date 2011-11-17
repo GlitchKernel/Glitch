09821ef51ed9e3c70d927e777c2593a94575de3a
Bill Crossley <billcrossley@gmail.com>
2011-11-17 07:13:16 -0500

    added verify_toolchain call to prepgsm.sh and prepcdma.sh

--- prepgsm.sh
+++ prepgsm.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# need these lines for make mrproper to work right
+source ./verify_toolchain.sh
+verify_toolchain
+
+export CROSS_COMPILE=$CROSS_COMPILE_GLITCH
+
 make mrproper
 
 echo "Old build cleaned"
