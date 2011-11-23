0f3a054c56f23d0e871b0961808a39673f03f1cb
Bill Crossley <billcrossley@gmail.com>
2011-11-23 10:54:27 -0500

    modified doit_scripts to use new screenstate scaling file

--- release/auto/doit_GSM.sh
+++ release/auto/doit_GSM.sh
@@ -28,7 +28,7 @@ REL=CM7${TYPE}-Glitch-DEV-$(date +%Y%m%d_%H%M)-OLDMODEM.zip
 	cd ../
 		find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 	cd release
-	cp 90screenstate_scaling system/etc/init.d/ || exit 1
+	cp S99screenstate_scaling system/etc/init.d/ || exit 1
 	cp 000leakage system/etc/init.d/ || exit 1
 	cp lights.aries.so.BLN system/lib/hw/lights.aries.so || exit 1
 	cp logcat_module system/etc/init.d/ || exit 1
