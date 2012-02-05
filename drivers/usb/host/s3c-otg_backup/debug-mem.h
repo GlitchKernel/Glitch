4af9e8e716fafc901c52d3b8ff9dff8e13414608
Tk Glitch <Ti3noU@gmail.com>
2012-02-06 00:17:08 +0100

    Cleanup

--- drivers/usb/host/s3c-otg_backup/debug-mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DEBUGMEM_H
-#define __DEBUGMEM_H
-
-// kevinh quick hack to see if the s3c stuff is doing memory properly
-
-void debug_alloc(void *addr);
-void debug_free(void *addr);
-
-#endif
